cf55084d2b917dd259fa996cd3b4bf252c93a906
kff11 <kff11@naver.com>
2021-07-11 13:27:11 +0900

    스프링부트 초기화

---
+++ src/main/java/com/klb/ojmj/OjmjApplication.java
@@ -0,0 +1,13 @@
+package com.klb.ojmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjmjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjmjApplication.class, args);
+    }
+
+}
