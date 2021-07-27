dff22baeb01479bc25ec2ccc8bc6462d9131c5ac
ChaeyBark <0913tkfkdgo@gmail.com>
2021-07-28 00:01:45 +0900

    #HSAE-24 랜덤 버튼 구현
    MainPage에 버튼 추가 및 버튼 선택 시 글자 변경 적용 완료.

--- webapp/src/Page/Main/MainPage.js
+++ webapp/src/Page/Main/MainPage.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-const MainPage = () => {
-    return (
-        <div>
-            <h2>연어좋아요</h2>    
-        </div>
-    );
+class MainPage extends React.Component {
+
+    constructor(state) {
+        super(state);
+
+        this.state = {
+            text: "연어 덮밥",
+        };
+    }
+
+    changeText = () => {
+        this.setState({
+            text: "고등어구이",
+        });
+    };
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.text}</h1>
+                <button onClick={this.changeText}>시작!</button>
+            </div>
+        );
+    }
 }
 
 export default MainPage;
