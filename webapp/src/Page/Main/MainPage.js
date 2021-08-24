1257db3c556a3fab09c37d706a422ef555afd190
ChaeyBark <0913tkfkdgo@gmail.com>
2021-08-24 23:08:38 +0900

    #HSAE-14 AJAX 세팅
    GET메소드를 통한 AJAX 통신 테스트 진행

--- webapp/src/Page/Main/MainPage.js
+++ webapp/src/Page/Main/MainPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 class MainPage extends React.Component {
 
@@ -16,11 +17,25 @@ class MainPage extends React.Component {
         });
     };
 
-    render(){
+    hitFood = () => {
+        axios.get('http://localhost:8080/api/food/KOR002')
+            .then(res => {
+                if(res.data === 1) {
+                    this.setState({
+                        text: "김치맛잇어요"
+                    });
+                }
+            }).catch(err => {
+            throw err;
+        });
+    }
+
+    render() {
         return (
             <div>
                 <h1>{this.state.text}</h1>
                 <button onClick={this.changeText}>시작!</button>
+                <button onClick={this.hitFood}>선택!</button>
             </div>
         );
     }
