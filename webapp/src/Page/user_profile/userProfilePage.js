2ed41a88efd83f60c7bb412b5d18add6317bf71b
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2021-12-27 22:08:13 +0900

    #HSAE-46 마이페이지
    - 마이페이지 화면 생성 및 Link 적용

---
+++ webapp/src/Page/user_profile/userProfilePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function userProfilePage() {
+    return (
+        <div>
+            <h1>마이페이지</h1>
+            <h4>닉네임</h4>
+            <h4>아이디</h4>
+            <h4>비밀번호</h4>
+        </div>
+    );
+}
+
+export default userProfilePage;
