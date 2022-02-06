a79ec20e550ce4e84365b1732131a61cbccc4bd5
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2022-02-06 16:32:47 +0900

    #HSAE-44 음식 리스트 화면 - 메뉴, 리뷰 CSS 변경 중...

--- webapp/src/Page/restaurant_detail/restaurantDetail.js
+++ webapp/src/Page/restaurant_detail/restaurantDetail.js
@@ -6,23 +6,34 @@ const restaurantDetailPage = () => {
     return (
         <div className={"restaurant_detail_page_body"}>
             <Header/>
-            <div className="restaurant_detail_content">
-                <div className="detail_header">
-                    <div className="restaurant_image"></div>
-                    <h3>선택한 식당 이름</h3>
-                    <h5>주소</h5>
-                    <h5>전화번호</h5>
-                </div>
-                <div>
-                    <h3>식당 설명</h3>
-                    <ul className="restaurant_explain">
-                        <li>
-                            <button>메뉴</button>
-                        </li>
-                        <li>
-                            <button>리뷰</button>
-                        </li>
-                    </ul>
+            <div className={"restaurant_content"}>
+                <div className={"restaurant_image"}></div>
+                <div className={"restaurant_explain"}>
+                    <div className={"restaurant_detail_explain"}>
+                        <h3>선택한 식당 이름</h3>
+                        <p>카테고리</p>
+                        <h5>주소</h5>
+                        <h5>전화번호</h5>
+                        <h3>식당 설명</h3>
+                    </div>
+                    <div className={"restaurant_breakdown"}>
+                        <ul className={"restaurant_breakdown"}>
+                            <li>
+                                <button>메뉴</button>
+                                <ul>
+                                    <li>연어덮밥</li>
+                                    <li>가츠동</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <button>리뷰</button>
+                                <ul>
+                                    <li>ㅇㅇㅇ님</li>
+                                    <li>ㅁㅁㅁ님</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
