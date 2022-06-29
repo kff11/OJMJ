0290fe6d5976ba1a6da97e1cb6df12bf49e31b59
ParkChaeyeong0 <0726tkfkdgo@naver.com>
2022-06-29 11:04:51 +0900

    #HSAE-51 페이지네이션 구현 - 페이지 수 설정 가능 - 첫 페이지 시, 이전 페이지 버튼 비활성화 - 마지막 페이지 시, 다음 페이지 버튼 비활성화

--- webapp/src/Page/admin/listRepetition.js
+++ webapp/src/Page/admin/listRepetition.js
@@ -1,8 +1,13 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {deleteFood, getFood} from "../../actions";
+import Pagination from "./adminComponent/Pagination";
 
 const ListRepetition = () => {
+    const [limit, setLimit] = useState(10); //페이지당 게시물 수
+    const [page, setPage] = useState(1); // 현재 페이지 번호
+    const offset = (page - 1) * limit; //첫 게시물의 위치
+
     const {foods} = useSelector(state => state.foodReducer);
 
     const dispatch = useDispatch();
@@ -24,34 +29,60 @@ const ListRepetition = () => {
     return (
         <div>
             <div>
-                <table className="food">
-                    <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>이름</th>
-                        <th>분류</th>
-                        <th>활성화<br/>상태</th>
-                        <th>선택<br/>횟수</th>
-                        <th>삭제</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {foods.map(rowData => (
-                        <tr key={rowData.id}>
-                            <td>{rowData.id}</td>
-                            <td>{rowData.name}</td>
-                            <td>{rowData.mainCategory}</td>
-                            <td>{rowData.status}</td>
-                            <td>{rowData.selectCount}</td>
-                            <td>
-                                <button onClick={() => onDelete(rowData.id)}>
-                                    삭제
-                                </button>
-                            </td>
+                <label>
+                    페이지 당 표시할 게시물 수:&nbsp;
+                    <select
+                        type="number"
+                        value={limit}
+                        onChange={({target: {value}}) => setLimit(Number(value))}
+                    >
+                        <option value="10">10</option>
+                        <option value="12">12</option>
+                        <option value="20">20</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                </label>
+
+                <main>
+                    <table className="food">
+                        <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>이름</th>
+                            <th>분류</th>
+                            <th>활성화<br/>상태</th>
+                            <th>선택<br/>횟수</th>
+                            <th>삭제</th>
                         </tr>
-                    ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        {foods.slice(offset, offset + limit).map(rowData => (
+                            <tr key={rowData.id}>
+                                <td>{rowData.id}</td>
+                                <td>{rowData.name}</td>
+                                <td>{rowData.mainCategory}</td>
+                                <td>{rowData.status}</td>
+                                <td>{rowData.selectCount}</td>
+                                <td>
+                                    <button onClick={() => onDelete(rowData.id)}>
+                                        삭제
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </main>
+
+                <footer>
+                    <Pagination
+                        total={foods.length}
+                        limit={limit}
+                        page={page}
+                        setPage={setPage}
+                    />
+                </footer>
             </div>
         </div>
     )
